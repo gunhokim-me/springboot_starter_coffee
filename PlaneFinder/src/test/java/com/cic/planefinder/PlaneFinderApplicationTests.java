d1ec6d1b4c2ebf055440e3d684eb55463bea1838
gunhokim-me <kgh3680@gmail.com>
2024-09-26 18:29:40 +0900

    Chapter6 planefinder

---
+++ PlaneFinder/src/test/java/com/cic/planefinder/PlaneFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cic.planefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
